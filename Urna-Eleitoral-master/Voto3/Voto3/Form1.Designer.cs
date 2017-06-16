3b618a8528f20e2bbfe82bffe7269746582c714c
Sozia <marceloresendevieira@hotmail.com>
2017-06-15 21:12:55 -0300

    Urna

---
+++ Urna-Eleitoral-master/Voto3/Voto3/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Voto3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnBranco = new System.Windows.Forms.Button();
+            this.btnCorrige = new System.Windows.Forms.Button();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.lblDigito1 = new System.Windows.Forms.Label();
+            this.lblDigito2 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.finalText = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn1, "btn1");
+            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.Name = "btn1";
+            this.btn1.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn2, "btn2");
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.Name = "btn2";
+            this.btn2.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn3, "btn3");
+            this.btn3.ForeColor = System.Drawing.Color.White;
+            this.btn3.Name = "btn3";
+            this.btn3.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn4, "btn4");
+            this.btn4.ForeColor = System.Drawing.Color.White;
+            this.btn4.Name = "btn4";
+            this.btn4.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn5, "btn5");
+            this.btn5.ForeColor = System.Drawing.Color.White;
+            this.btn5.Name = "btn5";
+            this.btn5.UseVisualStyleBackColor = false;
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn6, "btn6");
+            this.btn6.ForeColor = System.Drawing.Color.White;
+            this.btn6.Name = "btn6";
+            this.btn6.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn7, "btn7");
+            this.btn7.ForeColor = System.Drawing.Color.White;
+            this.btn7.Name = "btn7";
+            this.btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn8, "btn8");
+            this.btn8.ForeColor = System.Drawing.Color.White;
+            this.btn8.Name = "btn8";
+            this.btn8.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn9, "btn9");
+            this.btn9.ForeColor = System.Drawing.Color.White;
+            this.btn9.Name = "btn9";
+            this.btn9.UseVisualStyleBackColor = false;
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.btn0, "btn0");
+            this.btn0.ForeColor = System.Drawing.Color.White;
+            this.btn0.Name = "btn0";
+            this.btn0.UseVisualStyleBackColor = false;
+            // 
+            // btnBranco
+            // 
+            this.btnBranco.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.btnBranco, "btnBranco");
+            this.btnBranco.Name = "btnBranco";
+            this.btnBranco.UseVisualStyleBackColor = false;
+            // 
+            // btnCorrige
+            // 
+            this.btnCorrige.BackColor = System.Drawing.Color.Yellow;
+            resources.ApplyResources(this.btnCorrige, "btnCorrige");
+            this.btnCorrige.Name = "btnCorrige";
+            this.btnCorrige.UseVisualStyleBackColor = false;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.btnConfirmar, "btnConfirmar");
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            // 
+            // lblDigito1
+            // 
+            this.lblDigito1.BackColor = System.Drawing.Color.White;
+            this.lblDigito1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.lblDigito1, "lblDigito1");
+            this.lblDigito1.ForeColor = System.Drawing.Color.Black;
+            this.lblDigito1.Name = "lblDigito1";
+            // 
+            // lblDigito2
+            // 
+            this.lblDigito2.BackColor = System.Drawing.Color.White;
+            this.lblDigito2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.lblDigito2, "lblDigito2");
+            this.lblDigito2.ForeColor = System.Drawing.Color.Black;
+            this.lblDigito2.Name = "lblDigito2";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.BackColor = System.Drawing.Color.Black;
+            this.lblInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblInfo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            resources.ApplyResources(this.lblInfo, "lblInfo");
+            this.lblInfo.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Click += new System.EventHandler(this.lblInfo_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // finalText
+            // 
+            this.finalText.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.finalText, "finalText");
+            this.finalText.ForeColor = System.Drawing.Color.Chartreuse;
+            this.finalText.Name = "finalText";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Controls.Add(this.finalText);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblDigito2);
+            this.Controls.Add(this.lblDigito1);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.btnCorrige);
+            this.Controls.Add(this.btnBranco);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnBranco;
+        private System.Windows.Forms.Button btnCorrige;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label lblDigito1;
+        private System.Windows.Forms.Label lblDigito2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox finalText;
+    }
+}
+
